2f23366331224eb11c70b3cc0626da72a91da52b
Fraser Wilson <fraser@thewhitelion.eu>
2020-05-07 18:49:44 +0200

    Nazar.Views.Draw: new interface subprograms Viewport and Set_Viewport

--- src/nazar-views-draw.ads
+++ src/nazar-views-draw.ads
@@ -2,6 +2,16 @@ package Nazar.Views.Draw is
 
    type Draw_View_Interface is interface and Nazar_View_Interface;
 
+   function Viewport
+     (Draw_View : Draw_View_Interface)
+      return Rectangle
+      is abstract;
+
+   procedure Set_Viewport
+     (Draw_View : in out Draw_View_Interface;
+      Viewport  : Rectangle)
+   is abstract;
+
    type Nazar_Draw_View is access all Draw_View_Interface'Class;
 
 end Nazar.Views.Draw;
